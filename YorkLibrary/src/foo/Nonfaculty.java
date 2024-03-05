9b01e7387c545b1cdcb9248b9320ba79ffffe2e7
ghamsari-keyan <77293068+ghamsari-keyan@users.noreply.github.com>
2024-03-05 12:44:48 -0500

    Implemented Nonfaculty class
    just put basic functionality for this type of user

--- YorkLibrary/src/foo/Nonfaculty.java
+++ YorkLibrary/src/foo/Nonfaculty.java
@@ -1,8 +1,21 @@
 package foo;
 
-public class Nonfaculty extends User{
-	
+import java.util.ArrayList;
+
+public class Nonfaculty extends User {
+	private ArrayList<Course> courses;
+
 	public Nonfaculty() {
-		
+
 	}
+
+	public Nonfaculty(ArrayList<Course> courses) {
+		this.courses = courses;
+	}
+
+	//Getters
+	public ArrayList<Course> getCourses(){
+		return this.courses;
+	}
+
 }
