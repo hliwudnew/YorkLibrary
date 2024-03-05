e838227d7be8456259ed1ee747ad0bf5e7bd16f5
Kush Prajapati <10055925+Kush-Prajapati@users.noreply.github.com>
2024-03-05 01:58:31 -0500

    Implement Factory.java Setters

--- YorkLibrary/src/foo/Faculty.java
+++ YorkLibrary/src/foo/Faculty.java
@@ -18,23 +18,47 @@ public class Faculty extends User{
 	
 	//Setters
 	public void removeTextBook(Item textbook) {
-		//TODO: Oversimplification
-		this.textBooks.remove(textbook);
+
+		boolean exists = this.textBooks.contains(textbook);
+
+		if (exists && textbook != null) {
+			this.textBooks.remove(textbook);
+		} else if (!exists) {
+			System.out.println("Textbook cannot be removed: Doesn't Exist");
+		} else {
+			System.out.println("Textbook cannot be removed: Null Value");
+		}
 	}
 	
 	public void removeCourse(Course course) {
-		//TODO: Oversimplification
-		this.courses.remove(course);
+
+		boolean exists = this.courses.contains(course);
+
+		if (exists && course != null) {
+			this.courses.remove(course);
+		} else if (!exists) {
+			System.out.println("Course cannot be removed: Doesn't Exist");
+		} else {
+			System.out.println("Course cannot be removed: Null Value");
+		}
 	}
 	
 	public void addTextBook(Item textbook) {
-		//TODO: Oversimplification
-		this.textBooks.add(textbook);
+
+		if (textbook != null) {
+			this.textBooks.add(textbook);
+		} else {
+			System.out.println("Textbook cannot be added: Null Value");
+		}
 	}
 	
 	public void addCourse(Course course) {
-		//TODO: Oversimplification
-		this.courses.add(course);
+
+		if (course != null) {
+			this.courses.add(course);
+		} else {
+			System.out.println("Course cannot be added: Null Value");
+		}
 	}
 	
 	//Getters
